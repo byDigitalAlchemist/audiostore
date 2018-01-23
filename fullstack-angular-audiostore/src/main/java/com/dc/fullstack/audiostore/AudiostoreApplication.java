877b7a2e0f338b9aff4f2594eb609e76094b36e5
Anwaar <developer.anwar@gmail.com>
2018-01-23 14:03:06 -0500

    Project Audiostore
    Initial commit, Project Structure

---
+++ fullstack-angular-audiostore/src/main/java/com/dc/fullstack/audiostore/AudiostoreApplication.java
@@ -0,0 +1,12 @@
+package com.dc.fullstack.audiostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiostoreApplication.class, args);
+	}
+}
---
+++ fullstack-angular-audiostore/src/test/java/com/dc/fullstack/audiostore/FullstackAngularAudiostoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dc.fullstack.audiostore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FullstackAngularAudiostoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
