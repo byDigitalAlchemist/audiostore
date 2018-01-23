2dcd8aca2eba59ff30d9b0855716a057b3d42bc0
Anwaar <developer.anwar@gmail.com>
2018-01-23 15:06:32 -0500

    Model Entities added
    User class modified to add additional attributes

--- fullstack-angular-audiostore/src/main/java/com/dc/fullstack/audiostore/domain/security/User.java
+++ fullstack-angular-audiostore/src/main/java/com/dc/fullstack/audiostore/domain/security/User.java
@@ -1,5 +1,83 @@
 package com.dc.fullstack.audiostore.domain.security;
 
-public class User {
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class User implements UserDetails, Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id",nullable=false,updatable=false)
+	private Long Id;
+	
+	
+	private String userName;
+	private String userPass;
+	private String firstName;
+	private String lastName;
+	
+	private String email;
+	private String phone;
+	private boolean active = false;
+	
+	@OneToMany(mappedBy="user", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	@JsonIgnore
+	private Set<UserRole> userRoles = new HashSet<>();
+	
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
