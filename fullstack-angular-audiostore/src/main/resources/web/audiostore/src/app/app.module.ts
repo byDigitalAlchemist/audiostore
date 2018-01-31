88709021a1ffc6b2141ad7f2644fc0899f5b0234
Anwaar <developer.anwar@gmail.com>
2018-01-31 16:56:21 -0500

    Routing
    1- routing added to app
    2- module updated with routing

--- fullstack-angular-audiostore/src/main/resources/web/audiostore/src/app/app.module.ts
+++ fullstack-angular-audiostore/src/main/resources/web/audiostore/src/app/app.module.ts
@@ -3,28 +3,35 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatToolbarModule} from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule } from '@angular/material';
+
+import { Routing } from './app.routing';
 import 'hammerjs'; 
 
 
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { LoginService } from './service/login/login.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    Routing,
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule,MatToolbarModule 
+    MatButtonModule, MatCheckboxModule, MatToolbarModule 
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
