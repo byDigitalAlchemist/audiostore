db1508fdfc176575b3e42168482e5ee8b511e693
Anwaar <developer.anwar@gmail.com>
2018-02-06 14:37:54 -0500

    Disable Hibernate and Datasource initialization
    1- Database not created, therefore getting exceptions.

--- fullstack-angular-audiostore/src/main/java/com/dc/fullstack/audiostore/AudiostoreApplication.java
+++ fullstack-angular-audiostore/src/main/java/com/dc/fullstack/audiostore/AudiostoreApplication.java
@@ -2,9 +2,13 @@ package com.dc.fullstack.audiostore;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class AudiostoreApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
