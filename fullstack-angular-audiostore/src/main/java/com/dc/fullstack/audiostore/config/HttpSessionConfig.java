72859f61c9d2b3e7bbc7764a5c1882516c545f5e
Anwaar <developer.anwar@gmail.com>
2018-02-08 14:54:11 -0500

    Session Config
    1- Session Config updated

--- fullstack-angular-audiostore/src/main/java/com/dc/fullstack/audiostore/config/HttpSessionConfig.java
+++ fullstack-angular-audiostore/src/main/java/com/dc/fullstack/audiostore/config/HttpSessionConfig.java
@@ -1,15 +1,11 @@
 package com.dc.fullstack.audiostore.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 public class HttpSessionConfig {
 
-	@Bean
+	/*@Bean
 	public LettuceConnectionFactory connectionFactory(){
 		return new LettuceConnectionFactory();
-	}
+	}*/
 	
 }
